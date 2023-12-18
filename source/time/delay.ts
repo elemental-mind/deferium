2f5b5091ba5e6561628344d4d3320fe16302a633
Magnus Meseck <magnus.meseck@googlemail.com>
2023-12-18 03:42:37 +0100

    Add implementaton for delay

---
+++ source/time/delay.ts
@@ -0,0 +1,74 @@
+import { ProtectedAwaitable } from "../core/awaitable.js";
+
+export class Delay extends ProtectedAwaitable
+{
+    public creationTime;
+    protected timeout;
+    constructor(protected msDelay: number) { 
+        super();
+        this.creationTime = Date.now();
+        this.timeout = setTimeout(this.resolve, msDelay);
+    };
+
+    static for(msDelay: number)
+    {
+        return new Delay(msDelay);
+    }
+
+    static until(time: Date | number)
+    {
+        if(time instanceof Date)
+            return new Delay(time.getTime() - Date.now());
+        else 
+            return new Delay(time - Date.now());
+    }
+
+    get duration()
+    {
+        return this.msDelay;
+    }
+
+    get msElapsed()
+    {
+        return Date.now() - this.creationTime;
+    }
+
+    get msRemaining()
+    {
+        return this.creationTime + this.msDelay - Date.now();
+    }
+
+    get endTime()
+    {
+        return this.creationTime + this.msDelay;
+    }
+
+    abort()
+    {
+        clearTimeout(this.timeout);
+        this.reject();
+    }
+}
+
+export class AdjustableDelay extends Delay
+{
+    set duration(msDelay: number)
+    {
+        this.msDelay = msDelay;
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.resolve, Math.max(this.creationTime+msDelay - Date.now(), 0));
+    }
+
+    set endTime(msUnixEpochEnd: number)
+    {
+        this.msDelay = msUnixEpochEnd - this.creationTime;
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.resolve, Math.max(msUnixEpochEnd - Date.now(), 0));
+    }
+
+    reset()
+    {
+        clearTimeout(this.timeout);
+        setTimeout(this.resolve, this.msDelay);
+    }
+}
