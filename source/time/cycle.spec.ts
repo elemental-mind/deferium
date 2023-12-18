a9ca7fb4a280a8c805e8bbdf35a01a5cf663a0b8
Magnus Meseck <magnus.meseck@googlemail.com>
2023-12-18 06:21:25 +0100

    Wrap asserts in try catch to ensure cleanup when tests fail

--- source/time/cycle.spec.ts
+++ source/time/cycle.spec.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
 import { Cycle } from "./cycle.js";
-import {Sequential} from "unitium"
+import { Sequential } from "unitium";
 
 @Sequential
 export class CycleTests
@@ -8,50 +8,81 @@ export class CycleTests
     ShouldCreatePendingCycle()
     {
         const cycle = Cycle.pending(1000);
-        assert.equal(cycle.isPending, true);
-        cycle.destroy();
+        try
+        {
+            assert.equal(cycle.isPending, true);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     ShouldCreateRunningCycle()
     {
         const cycle = Cycle.running(1000);
-        assert.equal(cycle.isRunning, true);
-        cycle.destroy();
+        try
+        {
+            assert.equal(cycle.isRunning, true);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     ShouldStartPendingCycle()
     {
         const cycle = Cycle.pending(1000);
-        cycle.start();
-        assert.equal(cycle.isRunning, true);
-        cycle.destroy();
+        try
+        {
+            cycle.start();
+            assert.equal(cycle.isRunning, true);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     ShouldThrowWhenStartingRunningCycle()
     {
         const cycle = Cycle.running(1000);
-        assert.throws(() =>
+        try
         {
-            cycle.start();
-        });
-        cycle.destroy();
+            assert.throws(() =>
+            {
+                cycle.start();
+            });
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     async ShouldEmitTicksWhenRunning()
     {
         const cycle = Cycle.running(100);
         let ticks = 0;
-        cycle.onTick.subscribe(() => ticks++);
-        await new Promise(resolve => setTimeout(resolve, 350));
-        cycle.stop();
-        cycle.destroy();
-        assert.equal(ticks, 3);
+        try
+        {
+            cycle.onTick.subscribe(() => ticks++);
+            await new Promise(resolve => setTimeout(resolve, 350));
+            assert.equal(ticks, 3);
+            cycle.stop();
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     async ShouldBeAsyncIterable()
     {
         const cycle = Cycle.pending(100);
         const count = this.#consumeAsync(cycle);
+
         cycle.start();
         await new Promise(resolve => setTimeout(resolve, 350));
         cycle.stop();
@@ -71,9 +102,14 @@ export class CycleTests
     {
         const cycle = Cycle.pending(500);
 
-        assert.equal(cycle.period, 500);
-
-        cycle.destroy();
+        try
+        {
+            assert.equal(cycle.period, 500);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     ShouldGetCycleTimes()
@@ -81,23 +117,33 @@ export class CycleTests
         const now = Date.now();
         const cycle = Cycle.running(1000);
 
-        assert(cycle.lastCycleStart! - now < 10);
-        assert(cycle.nextCycleStart! >= now + 1000);
-
-        cycle.destroy();
+        try
+        {
+            assert(cycle.lastCycleStart! - now < 10);
+            assert(cycle.nextCycleStart! >= now + 1000);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 
     async ShouldGetCycleDurations()
     {
         const cycle = Cycle.running(500);
 
-        await new Promise(resolve => setTimeout(resolve, 300));
+        try
+        {
+            await new Promise(resolve => setTimeout(resolve, 300));
 
-        assert(cycle.msSinceLastCycleStart! >= 290);
-        assert(cycle.msSinceLastCycleStart! <= 310);
-        assert(cycle.msBeforeNextCycleStart! >= 190);
-        assert(cycle.msBeforeNextCycleStart! <= 210);
-
-        cycle.destroy();
+            assert(cycle.msSinceLastCycleStart! >= 270);
+            assert(cycle.msSinceLastCycleStart! <= 330);
+            assert(cycle.msBeforeNextCycleStart! >= 170);
+            assert(cycle.msBeforeNextCycleStart! <= 230);
+        }
+        finally
+        {
+            cycle.destroy();
+        }
     }
 }
